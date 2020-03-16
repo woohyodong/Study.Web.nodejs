99b4cc9e108d37a8a20ec27254b5f01ec3ea7804
woojjajja <woohyodong@gmail.com>
2020-03-16 18:06:11 +0900

    2 commit

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var express = require('express');
+
+//var app = express.createServer(express.logger());
 var app = express();
 
-app.get('/', function(req,res){
-    res.send('Hello Woojjajja');
-})
+app.get('/', function(req, res) {
+    res.send('Hello World!'); 
+});
 
-var server = app.listen(3000,function(){
-    console.log("Server is running!");
-})
+var port = process.env.PORT || 5000;
+
+app.listen(port, function() { 
+    console.log("Listening on " + port); 
+});
